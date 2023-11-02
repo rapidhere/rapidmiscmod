d5d318528a11e26f368df841948ca8acf0d3c820
dongqi.rapid <dongqi.rapid@bytedance.com>
2023-11-03 05:10:58 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+just a mindustry mod
---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const resetSpawnedUnit = (event) => {
+  var unit = event.unit;
+  
+  if (unit.isFlying()) {
+    unit.controller(new FlyingAI());
+  }
+};
+
+Events.on(UnitSpawnEvent, setup);
