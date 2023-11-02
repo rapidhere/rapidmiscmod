ea44b0dbd84440eb71128f41b5e5d1f68f5a9889
dongqi.rapid <dongqi.rapid@bytedance.com>
2023-11-03 05:15:36 +0800

    init commit2

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
 const resetSpawnedUnit = (event) => {
   var unit = event.unit;
+  Vars.ui.hudfrag.showToast("spawnunit!");
   
   if (unit.isFlying()) {
+    Vars.ui.hudfrag.showToast("flyingunit!");
     unit.controller(new FlyingAI());
+    Vars.ui.hudfrag.showToast("flyingunit done!");
   }
 };
 
