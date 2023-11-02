cb6dfdcad29be8bcbd9ec6aba7079252127f4ab2
dongqi.rapid <dongqi.rapid@bytedance.com>
2023-11-03 05:23:33 +0800

    disable FlyingFollowingAI for flying units

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,8 @@
 const resetSpawnedUnit = (event) => {
   var unit = event.unit;
-  Vars.ui.hudfrag.showToast("spawnunit!");
   
   if (unit.isFlying()) {
-    Vars.ui.hudfrag.showToast("flyingunit!");
     unit.controller(new FlyingAI());
-    Vars.ui.hudfrag.showToast("flyingunit done!");
   }
 };
 
